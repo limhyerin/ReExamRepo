525448a98ccd3b590b63b8a0c0407f01759d4c13
limhyerin <rin091345@gmail.com>
2024-01-29 13:55:55 +0900

    수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
   const students = [
@@ -8,13 +9,17 @@ function App() {
     { name: "Diana", age: 19, grade: "D" },
   ];
 
+  const [minAge, setMinAge] = useState(18);
+
+  const inputMinAge = event => {
+    setMinAge(event.target.value);
+  }
+
   // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter((student) => student.age >= 18).map((item) => {
-    return item.name;
-  })
+  const filteredStudents = students.filter((student) => student.age >= minAge);
 
   // TODO: map을 사용하여 필터링된 학생들의 정보를 표시하세요.
-  const ageGrade = filteredStudents.map((student, index) => {
+  const onClickAgeGrade = filteredStudents.map((student, index) => {
     <li key ={index}>
       Age : {student.age}, Grade : {student.grade}
     </li>
@@ -25,9 +30,13 @@ function App() {
       <h1>학생 목록</h1>
       <ul>
         {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
-        <div onClick={ageGrade}>{filteredStudents}</div>
+        {filteredStudents.map((student, index) => {
+          <li key ={index}>
+            name : {student.name}
+          </li>
+        })}
         {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
-        <p onClick={ageGrade}></p>
+        <button onClick={onClickAgeGrade} value={minAge} onChange={inputMinAge}>{}</button>
       </ul>
     </div>
   );
